393d2347ab8ea8c256802a8c0567ac4dfcbf959e
Katukusumanth333 <157560630+Katukusumanth333@users.noreply.github.com>
2024-10-07 13:37:47 +0530

    Add files via upload

---
+++ Day 3 programm 1.py
@@ -0,0 +1,27 @@
+empty_list=[]
+print("Test case 1:An empty list")
+print("Input:",empty_list)
+print("Expected Output:",empty_list)
+print()
+
+single_element_list=[2]
+print("Test case 2:A list with one element")
+print("input:",single_element_list)
+print("Expected output:",single_element_list)
+print()
+
+
+identical_elements_list=[7,7,7,7]
+print("Test case 3: A list with all identical elements")
+print("in[put:",identical_elements_list)
+print("Expected output:",identical_elements_list)
+print()
+
+
+negative_numbers_list=[-1,-2,-3,-4]
+print("Test case 4:A list with all negative numbers")
+print("input:",negative_numbers_list)
+print("expected output:",negative_numbers_list)
+print()
+
+
